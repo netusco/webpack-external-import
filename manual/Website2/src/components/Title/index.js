eeee369e6a6ea9103869111f15263d15e86e3c0f
ScriptedAlchemy <zjackson@lululemon.com>
2020-01-09 18:52:19 -0800

    fix: fixing ref errors

--- manual/Website2/src/components/Title/index.js
+++ manual/Website2/src/components/Title/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import 'react-select';
 
-export const Title = React.forwardRef(({ title }, ref) => (
-  <h1 ref={ref}>
-TITLE COMPONENT:
-    {title}
-  </h1>
-));
+export const Title = React.forwardRef(({ title }, ref) => {
+  console.log('interleaved ref', ref);
+  return (
+    <h1 ref={ref}>
+        TITLE COMPONENT:
+      {title}
+    </h1>
+  );
+});
 
 /* externalize:TitleComponent */
